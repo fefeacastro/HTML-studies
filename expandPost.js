5fa30867ff22badddddb45936f995c37dc5f09f2
Fernanda Castro <fefeacastro@hotmail.com>
2020-07-24 12:58:58 -0300

    expanding post on click

---
+++ expandPost.js
@@ -0,0 +1,8 @@
+function openPost(postTitle) {
+    var i, x;
+    x = document.getElementsByClassName("containerTab");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(postTitle).style.display = "block";
+}
